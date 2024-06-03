b6cd1717c52a1d78a50440fa70c1535eb9f3df05
p2006609 <yanis.aumasson@etu.univ-lyon1.fr>
2024-06-04 01:05:59 +0200

    physics working (maybe needs some adjustments

---
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+  
+    private Vector3 offset = new Vector3(0, 0, -10);
+    private float smoothSpeed = 0.25f;
+    private Vector3 velocity = Vector3.zero;
+
+    [SerializeField] private Transform player;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 desiredPosition = player.position + offset;
+        Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+}
--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -2,13 +2,22 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public Rigidbody2D rb;
+    public Rigidbody2D rbPlayer;
+    public Rigidbody2D rbCross;
     public float speed = 6000.0f; // seems to be a good speed
     public float rotationSpeed = 10.0f; // [5 - 15]
 
+    // private Vector2 initialOffset;
+
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        // rbPlayer and rbCross should be assigned via the Inspector or by finding the specific child components
+        rbPlayer = GetComponent<Rigidbody2D>();
+
+        // // Assuming rbCross is a child of the player, you should assign it correctly
+        // rbCross = transform.Find("Cross").GetComponent<Rigidbody2D>();
+
+        // initialOffset = rbCross.position - rbPlayer.position;
     }
 
     void FixedUpdate()
@@ -16,17 +25,23 @@ public class NewBehaviourScript : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveHorizontal, moveVertical).normalized;
-        rb.AddForce(movement * speed * Time.deltaTime);
+        rbPlayer.AddForce(movement * speed * Time.fixedDeltaTime); // Use Time.fixedDeltaTime for FixedUpdate
 
         float mouseX = Input.GetAxis("Mouse X") * 100;
 
         if (Mathf.Abs(mouseX) > 1f)
         {
-            rb.MoveRotation(rb.rotation - mouseX * rotationSpeed * Time.deltaTime);
-        } else
+            rbPlayer.MoveRotation(rbPlayer.rotation - mouseX * rotationSpeed * Time.fixedDeltaTime);
+        }
+        else
         {
-            rb.angularVelocity = 0;
+            rbPlayer.angularVelocity = 0;
         }
 
+        // Vector2 newCrossPosition = rbPlayer.position + (Vector2)(Quaternion.Euler(0, 0, rbPlayer.rotation) * initialOffset);
+
+        // // Ensure the cross follows the player (parent) like in kinematic mode
+        // rbCross.MovePosition(newCrossPosition);
+        // rbCross.MoveRotation(rbPlayer.rotation);
     }
 }
